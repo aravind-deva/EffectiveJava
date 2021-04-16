d72d206c29811a597e7d1e1581e57bfa5521fb6c
aravind-deva <78798318+aravind-deva@users.noreply.github.com>
2021-04-16 14:44:34 +0530

    refactor:Moving test source files to test package
    The test files are repackaged to test directory to align with better source code management

--- test/serialization/SerializationProxyPattTest.java
+++ test/serialization/SerializationProxyPattTest.java
@@ -6,19 +6,20 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-public class NonSerializableSuperTest {
+public class SerializationProxyPattTest {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		SerializableSubClass sp = new SerializableSubClass("Phoenix", "5A", 1, "Aravind");
-		FileOutputStream serializationFile = new FileOutputStream("D:\\myserialize.ser");
+		SerializationProxyPattern sp = new SerializationProxyPattern("Aravind");
+		FileOutputStream serializationFile = new FileOutputStream("C:\\myserialize.ser");
 		ObjectOutputStream oos = new ObjectOutputStream(serializationFile);
 		oos.writeObject(sp);
 		oos.close();
-		FileInputStream sFile = new FileInputStream("D:\\myserialize.ser");
+		FileInputStream sFile = new FileInputStream("C:\\myserialize.ser");
 		ObjectInputStream ois = new ObjectInputStream(sFile);
-		SerializableSubClass sp2 = (SerializableSubClass) ois.readObject();
+		SerializationProxyPattern sp2 = (SerializationProxyPattern) ois.readObject();
 		ois.close();
-		System.out.println("From Deserialization" + sp2.toString());
+		System.out.println("From Deserialization" + sp2.getMyName());
+
 	}
 
 }
